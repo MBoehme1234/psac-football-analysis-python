297e6893a8e9cb3360cc3fab1392f2d38dd2148e
Doug Boehme <dougr@boehme.us>
2025-04-21 20:00:20 -0400

    chore: change to port 443

--- python-app/src/page.py
+++ python-app/src/page.py
@@ -553,4 +553,4 @@ def detect():
         }), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 443)))
